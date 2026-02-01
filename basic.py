71a9bcbae37e3ce56ead7dff0c31f2c02d8fa72d
babybos99 <133647043+babybos99@users.noreply.github.com>
2026-02-01 14:53:08 +0700

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,37 @@
+# x = "299" # //kalau di * itu berarti 299 di ulang misalnya nilai z berarti 299 di looping 7x berarti 299 299 299 sampai 7x bukan hasil dari 299 x 7 (( di gabung ))
+# y = 299
+# z = 7
+
+# print(y * z)
+
+# mengubah data (casting)
+
+# string -> angka
+# x = "20"
+# y = int(x)
+
+# # angka -> string
+# umur = 20
+# maka = str(umur)
+# # hasil
+# print(x)
+# print(maka)
+
+# # input dari user
+# kota = input("Masukan nama kota")
+# jawa = ("suku")
+# print(kota)
+# print("jawa adalah", jawa)
+
+# print var
+# nama = "dho"
+# print (f"nama saya { nama }" ) // lebih keren
+# print ( 'nama saya', nama )
+
+# menyempurnakan
+
+nama = input("masukan nama :")
+umur = int(input("masukan umur :"))
+tempat_lahir = input("masukan tempat lahir :")
+
+print(f"Nama saya { nama }, Umur saya { umur }, tempat lahir saya{tempat_lahir}" )
